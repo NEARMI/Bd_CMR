c5a756ea1cf14a66a08674893a78811312e0f19c
morgankain <kainm@mcmaster.ca>
2022-04-13 14:05:39 -0400

    Other files needed for yeti jobs of all individual populations

---
+++ CMR_fits.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+export IFS=","
+
+cat which_model_fit.csv | while read a b c; do 
+
+job_file="CMR_${a}${b}_pop.job"
+
+echo "#!/bin/bash
+#SBATCH  -p normal
+#SBATCH --account=nearmi
+#SBATCH --nodes=1               
+#SBATCH --ntasks-per-node=3      
+#SBATCH --time=72:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mkain@usgs.gov
+#SBATCH --output=CMR_${a}${b}pop.log
+
+ml R/4.1.1
+Rscript top_level_script.R "$a" "$b" 1" > $job_file
+
+    sbatch $job_file
+
+done
+
+
+
+
+
+
+
+
